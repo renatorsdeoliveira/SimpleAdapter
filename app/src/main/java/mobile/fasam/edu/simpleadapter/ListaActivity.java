143998431ceaf86d3f5a954cdced04025adc1b37
renatorsdeoliveira <renatorsdeoliveira@gmail.com>
2019-10-10 22:08:57 -0300

    Criado a atividade

---
+++ app/src/main/java/mobile/fasam/edu/simpleadapter/ListaActivity.java
@@ -0,0 +1,14 @@
+package mobile.fasam.edu.simpleadapter;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ListaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lista);
+    }
+}
